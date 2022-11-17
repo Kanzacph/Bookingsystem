b348765df8477639da7d53933cdbbaf7d8c0590f
Kanzacph <91204491+Kanzacph@users.noreply.github.com>
2022-11-17 13:39:51 +0100

    Lavet CSS-fil til navbaren, TheNavbar.css. Logo virker nu.

--- src/Components/molecules/TheNavbar.jsx
+++ src/Components/molecules/TheNavbar.jsx
@@ -1,8 +1,11 @@
 import cphlogo from "../atoms/cphlogo.png";
+import './TheNavbar.css';
 
 export default function TheNavbar(props){
     return(
-        <img src={cphlogo.png} width="40px" alt="cphlogo"/>
+        <div className ="App-header">
+            <img src={cphlogo} width="200px" alt="cphlogo"/>
+        </div>
     )
 }
 

a2594c6514cbe6bdcd970f369ea0c96700ee9f8a
Victoriaronne <91202246+Victoriaronne@users.noreply.github.com>
2022-11-18 12:48:08 +0100

    Implementering af routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as React from "react";
 import TheNavbar from "./Components/molecules/TheNavbar.jsx";
 import MainContent from "./Components/molecules/MainContent.jsx";
 
-
 function App() {
   return (
-    <div className='app-container'>
+    <div className="app-container">
       <TheNavbar />
       <MainContent />
     </div>
--- src/Components/molecules/MainContent.jsx
+++ src/Components/molecules/MainContent.jsx
@@ -1,14 +1,18 @@
-import './MainContent.css';
-import Frontpage from './Frontpage.jsx';
+import "./MainContent.css";
+import Frontpage from "./Frontpage.jsx";
+import NewBooking from "./NewBooking.jsx";
+import * as React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-export default function MainContent(props){
-    return(
-        <div className ="main-content-container">
-            <Frontpage />
-            {/* <Routes>
-                <Route path='/' element={<Frontpage />}></Route>
-                <Route path='new-booking' element={<NewBooking />}></Route>
-            </Routes> */}
-        </div>
-    )
+export default function MainContent(props) {
+  return (
+    <div className="main-content-container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Frontpage />}></Route>
+          <Route path='new-booking' element={<NewBooking />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }

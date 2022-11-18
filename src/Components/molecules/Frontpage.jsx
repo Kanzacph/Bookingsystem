37efe1dd2ef3568fcea8a140dd908655015efac1
Victoriaronne <91202246+Victoriaronne@users.noreply.github.com>
2022-11-18 16:01:49 +0100

    Made buttons work

--- src/Components/molecules/Frontpage.jsx
+++ src/Components/molecules/Frontpage.jsx
@@ -1,26 +1,32 @@
-import './Frontpage.css';
+import "./Frontpage.css";
 import Kalender from "../atoms/Kalender.png";
 import Booking from "../atoms/Booking.png";
+import { useNavigate } from "react-router";
 
-export default function Frontpage(props){
-    return(
-        <div className ="frontpage-container">
-            <h1>Vælg næste trin</h1>
-            <h3>Vil du booke et nyt lokale eller se dine bookinger</h3>
-            <div className='frontpage-button-container'>
-                <div className='frontpage-button'>
-                     <img className='images' src={Kalender} alt="Kalender"/> 
-                     <p>
-                        Book lokale
-                     </p>
-                     </div>
-                <div className='frontpage-button'> 
-                <img className='images1' src={Booking} alt="Booking"/> 
-                <p>
-                    Mine bookinger
-                </p>
-                </div>
-            </div>
+export default function Frontpage(props) {
+  const navigate = useNavigate();
+
+  function handleClick(route) {
+    navigate(route);
+  }
+
+  return (
+    <div className="frontpage-container">
+      <h1>Vælg næste trin</h1>
+      <h3>Vil du booke et nyt lokale eller se dine bookinger</h3>
+      <div className="frontpage-button-container">
+        <div
+          onClick={() => handleClick("/new-booking")}
+          className="frontpage-button"
+        >
+          <img className="images" src={Kalender} alt="Kalender" />
+          <p>Book lokale</p>
         </div>
-    )
+        <div onClick={() => handleClick('/my-bookings')} className="frontpage-button">
+          <img className="images1" src={Booking} alt="Booking" />
+          <p>Mine bookinger</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Components/molecules/MainContent.jsx
+++ src/Components/molecules/MainContent.jsx
@@ -2,6 +2,8 @@ import "./MainContent.css";
 import Frontpage from "./Frontpage.jsx";
 import NewBooking from "./NewBooking.jsx";
 import NewBooking2 from "./NewBooking2.jsx";
+import MyBookings from "./MyBookings.jsx";
+import MyBookings2 from "./MyBookings2.jsx";
 import * as React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
@@ -13,6 +15,8 @@ export default function MainContent(props) {
           <Route path="/" element={<Frontpage />}></Route>
           <Route path='new-booking' element={<NewBooking />}></Route>
           <Route path='new-booking2' element={<NewBooking2 />}></Route>
+          <Route path='my-bookings' element={<MyBookings />}></Route>
+          <Route path='my-bookings2' element={<MyBookings2 />}></Route>
         </Routes>
       </BrowserRouter>
     </div>

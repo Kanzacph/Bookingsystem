9a233ae1339567bd46ea380d950bb08963c56dda
nataligz <91202367+nataligz@users.noreply.github.com>
2022-11-18 13:07:27 +0100

    Designet frontpage færdig

--- src/Components/molecules/Frontpage.jsx
+++ src/Components/molecules/Frontpage.jsx
@@ -1,4 +1,6 @@
 import './Frontpage.css';
+import Kalender from "../atoms/Kalender.png";
+import Booking from "../atoms/Booking.png";
 
 export default function Frontpage(props){
     return(
@@ -6,8 +8,18 @@ export default function Frontpage(props){
             <h1>Vælg næste trin</h1>
             <h3>Vil du booke et nyt lokale eller se dine bookinger</h3>
             <div className='frontpage-button-container'>
-                <div className='frontpage-button'></div>
-                <div className='frontpage-button'></div>
+                <div className='frontpage-button'>
+                     <img className='images' src={Kalender} alt="Kalender"/> 
+                     <p>
+                        Book lokale
+                     </p>
+                     </div>
+                <div className='frontpage-button'> 
+                <img className='images1' src={Booking} alt="Booking"/> 
+                <p>
+                    Mine bookinger
+                </p>
+                </div>
             </div>
         </div>
     )

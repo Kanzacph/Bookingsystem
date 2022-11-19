20934c673b6d31fe8293216e72a6f728754c6cea
nataligz <91202367+nataligz@users.noreply.github.com>
2022-11-19 16:29:20 +0100

    Ændret i frontpage

--- src/Components/molecules/Frontpage.jsx
+++ src/Components/molecules/Frontpage.jsx
@@ -6,23 +6,23 @@ import { useNavigate } from "react-router";
 export default function Frontpage(props) {
   const navigate = useNavigate();
 
+
   function handleClick(route) {
     navigate(route);
   }
 
-export default function Frontpage(props){
     return(
         <div className ="frontpage-container">
             <h1>Vælg næste trin</h1>
             <h3>Vil du booke et nyt lokale eller se dine bookinger</h3>
             <div className='frontpage-button-container'>
-                <div className='frontpage-button'>
+                <div className='frontpage-button'onClick={() => handleClick('/new-booking')}>
                      <img className='images' src={Kalender} alt="Kalender"/> 
                      <p>
                         Book lokale
                      </p>
                      </div>
-                <div className='frontpage-button'> 
+                <div className='frontpage-button' onClick={() => handleClick('/my-bookings')}> 
                 <img className='images1' src={Booking} alt="Booking"/> 
                 <p>
                     Mine bookinger

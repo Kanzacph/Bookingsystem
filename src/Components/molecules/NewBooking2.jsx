d3b6fe97e9dab257f96655159e6b775c830041d4
nataligz <91202367+nataligz@users.noreply.github.com>
2022-11-21 10:53:28 +0100

    Ny booking - liste over lokaler

--- src/Components/molecules/NewBooking2.jsx
+++ src/Components/molecules/NewBooking2.jsx
@@ -1,9 +1,30 @@
-import "./NewBooking2";
+import './NewBooking2.css'
 
 export default function NewBooking2(props){
     return(
         <div>
-            <h1 className="overskrift">Liste over lokaler</h1>
+            <h1>Liste over lokaler</h1>
+            <div className='local-buttons-container'>
+            <div className='local-buttons'>
+                <p>
+                    Lokale 101
+                </p>
+                <p>
+                    Lokale 102
+                </p>
+                <p>
+                    Lokale 103
+                </p>
+                <p>
+                    Lokale 104
+                </p>
+                <p>
+                    Lokale 105
+                </p>
+            </div>
+            </div>
+
+            
         </div>
     )
 }

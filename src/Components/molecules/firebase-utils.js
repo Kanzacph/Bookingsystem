7e1feb55aafd42e8d0d863e1047d2ef070ed5478
Kanzacph <91204491+Kanzacph@users.noreply.github.com>
2022-11-22 11:03:14 +0100

    Firebase transforming

--- src/Components/molecules/firebase-utils.js
+++ src/Components/molecules/firebase-utils.js
@@ -1,1 +1,15 @@
 // Firebase
+
+export function transformToArray(data) {
+    console.log(data)
+    const toReturn = [];
+    for (let key in data) {
+      const temp = {
+        ...data[key],
+        id: key,
+      };
+      toReturn.push(temp);
+    }
+    return toReturn;
+
+}

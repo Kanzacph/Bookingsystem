f25d737cf754225f123fb8caaa9762667749e8a7
Victoriaronne <91202246+Victoriaronne@users.noreply.github.com>
2022-11-30 13:53:37 +0100

    Forsøg på at lave new booking

--- src/Components/molecules/NewBooking.jsx
+++ src/Components/molecules/NewBooking.jsx
@@ -1,39 +1,61 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { transformToArray } from "./firebase-utils";
 import "./NewBooking.css";
-import './firebase-utils';
+import "./Newbooking_singlebooking";
 
 export default function NewBooking(props) {
+  const [selectedDate, setSelectedDate] = useState('');
+  const [availableRooms, setAvailableRooms] = useState([]);
+
+  const getRooms = async () => {
+    const response = await fetch(
+      `https://frontend-eksamensprojekt-default-rtdb.europe-west1.firebasedatabase.app/rooms.json`,
+      {
+        method: "GET",
+      }
+    );
+    setAvailableRooms(transformToArray(await response.json()));
+  };
+
+  const handleDate = (e) => {
+    setSelectedDate(e.target.value);
+    getRooms();
+  };
+
   return (
-    <div> 
-      
-      <form action="">
-        <label for="birthday:"></label>
-        <input type="date" id="birthday" name="birthday" />
-        <input type="submit" />
-      </form>
+    <div className="booking">
+      <h1>Book et lokale</h1>
+      <div className="calender">
+        <form action="">
+          <div>
+            <input
+              className="datepicker calender-img"
+              type="date"
+              id="calender"
+              name="calender"
+              onChange={handleDate}
+            />
+          </div>
+        </form>
+      </div>
 
-      <div className="mybookings-boks2">
-    <div className="myboookings-boks1">
-        <div className="mybookings-date">
-            <h1>
-                <div className="datoen"></div>
-            </h1>
-        </div>
-        <div className="mybookings-info">
-            <p className="lokale"></p>
-            <p className="tidspunkt">08:00 - 12:00</p>
-            <p className="pladser">40 Pladser</p>
-        </div>
+      {availableRooms.map((room) => {
+        return (
+          <div className="mybookings-boks2">
+            <div className="myboookings-boks1">
+              <div className="mybookings-date">
+                <h1>
+                  <div className="datoen">{room.id}</div>
+                </h1>
+              </div>
+              <div className="mybookings-info">
+                <p className="lokale">{selectedDate}</p>
+                <p className="pladser">{room.seats}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
-
-
-</div> 
-
-    </div>
-
-    
   );
-
-
- 
 }

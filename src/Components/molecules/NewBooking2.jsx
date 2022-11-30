b0d2ac958340ebd65e7b9113d9d3b3a039b0974a
Kanzacph <91204491+Kanzacph@users.noreply.github.com>
2022-11-30 14:24:02 +0100

    css ændringer

--- src/Components/molecules/NewBooking2.jsx
+++ src/Components/molecules/NewBooking2.jsx
@@ -10,23 +10,19 @@ export default function NewBooking2(props){
     return(
         <div className='local-buttons-container'>
             <h1>Booking</h1>
-            <div>
-            <div className='local-buttons'>
-                <h2>Liste over lokaler</h2>
-                <li>Lokale 101</li>
-                <li>Lokale 102</li>
-                <li>Lokale 203</li>
-                <li>Lokale 204</li>
-                
-                <div className='mere' onClick={() => handleClick('/locals')}  >
+
+                    <li>Lokale 101</li>
+                    <li>Lokale 102</li>
+                    <li>Lokale 203</li>
+                    <li>Lokale 204</li>
+
+            <div className='mere' onClick={() => handleClick('/locals')}  >
                 <p>Læs om lokalerne her</p>
-                </div>
             </div>
-            </div>
-
-            
-
-            
+     
         </div>
+
+
+            
     )
 }

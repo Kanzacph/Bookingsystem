cca2601231743337c98cb631fbc8507fa5750927
nataligz <91202367+nataligz@users.noreply.github.com>
2022-12-05 14:58:27 +0100

    tilføjet delete og update knapper

--- src/Components/molecules/SingleBooking.jsx
+++ src/Components/molecules/SingleBooking.jsx
@@ -1,68 +1,68 @@
-// Kanza
-import { useState } from "react";
-import changeicon from "../atoms/change-ikon.png";
-import deleteicon from "../atoms/delete-ikon.png";
-import DeleteBooking from "./Delete";
-import UpdateBooking from "./Update";
-import NewBooking from "./NewBooking";
-import { useEffect} from "react";
+// // Kanza
+// import { useState } from "react";
+// import changeicon from "../atoms/change-ikon.png";
+// import deleteicon from "../atoms/delete-ikon.png";
+// import DeleteBooking from "./Delete";
+// import UpdateBooking from "./Update";
+// import NewBooking from "./NewBooking";
+// import { useEffect} from "react";
 
 
-export default function SingleBooking({ booking }) {
-  const [selectedDate, setSelectedDate] = useState('');
-  const [availableRooms, setAvailableRooms] = useState([]);
-  const [timeslot, setTimeslot] = useState("");
+// export default function SingleBooking({ booking }) {
+//   const [selectedDate, setSelectedDate] = useState('');
+//   const [availableRooms, setAvailableRooms] = useState([]);
+//   const [timeslot, setTimeslot] = useState("");
 
 
-  const deleteBooking = async() => {
+//   const deleteBooking = async() => {
     
-      const response = await fetch(
-      `https://frontend-eksamensprojekt-default-rtdb.europe-west1.firebasedatabase.app/bookings/${booking.id}.json`,
-        {
-          method: "DELETE",
-          body: JSON.stringify(booking),
-        }
-        );
-  }
+//       const response = await fetch(
+//       `https://frontend-eksamensprojekt-default-rtdb.europe-west1.firebasedatabase.app/bookings/${booking.id}.json`,
+//         {
+//           method: "DELETE",
+//           body: JSON.stringify(booking),
+//         }
+//         );
+//   }
 
 
   
-  return (
-      <div> 
+//   return (
+//       <div> 
         
-        <div className="mybookings-boks2">
-      <div className="myboookings-boks1">
-          <div className="mybookings-date">
-              <h1>
-                  <div className="datoen">{booking.date}</div>
-              </h1>
-          </div>
-          <div className="mybookings-info">
-              <p className="lokale">{booking.room}</p>
-              <p className="tidspunkt">{booking.time}</p>
-              <p className="pladser">40 Pladser</p>
-          </div>
-          <div className="mybookings-buttons">
+//         <div className="mybookings-boks2">
+//       <div className="myboookings-boks1">
+//           <div className="mybookings-date">
+//               <h1>
+//                   <div className="datoen">{booking.date}</div>
+//               </h1>
+//           </div>
+//           <div className="mybookings-info">
+//               <p className="lokale">{booking.room}</p>
+//               <p className="tidspunkt">{booking.time}</p>
+//               <p className="pladser">40 Pladser</p>
+//           </div>
+//           <div className="mybookings-buttons">
 
-            <div className='changebooking-button'> 
+//             <div className='changebooking-button'> 
 
-          <UpdateBooking updateBooking={booking}/>
+//           <UpdateBooking updateBooking={booking}/>
 
-          </div>
+//           </div>
 
-            <div className='deletebooking-button'> 
-          <button onClick={deleteBooking} className="delete-button"><img className="deletebooking-image"src={deleteicon}/></button>
+//             <div className='deletebooking-button'> 
+//           <button onClick={deleteBooking} className="delete-button"><img className="deletebooking-image"src={deleteicon}/></button>
 
-          </div>
+//           </div>
 
-          </div>
-      </div>
+//           </div>
+//       </div>
 
 
-  </div> 
+//   </div> 
 
-      </div>
+//       </div>
 
       
-    );
-  }
+//     );
+//   }

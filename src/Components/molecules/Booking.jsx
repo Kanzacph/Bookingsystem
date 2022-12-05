d529066f4349390aa2cce3d3ca55d94531aba1cc
Victoriaronne <91202246+Victoriaronne@users.noreply.github.com>
2022-12-05 16:01:59 +0100

    Lagt delete over i sin egen fil

--- src/Components/molecules/Booking.jsx
+++ src/Components/molecules/Booking.jsx
@@ -1,44 +1,32 @@
-import './CSS/NewBooking.css'
-import update from '../atoms/update.png';
-import deleteButton from '../atoms/deleteButton.png'
+import "./CSS/NewBooking.css";
+import update from "../atoms/update.png";
+import deleteButton from "../atoms/deleteButton.png";
+import Delete from "./Delete";
 
-export default function Booking({timeslot, room, date, id}) {
-    const handleDelete = async () => {
-        const response = await fetch(
-        `https://frontend-eksamensprojekt-default-rtdb.europe-west1.firebasedatabase.app/bookings/${id}.json`,
-        {
-            method: "DELETE"
-        }
-        )
-        window.location.reload()
-    }
-    return (
-        <div className="mybookingsBox">
-            <div className="newboooking">
-                <div className="mybookings-date">
-                    <h1>{room}</h1>
-                </div>
-
-                <div className="mybookings-info">
-                    <p className='date'> Dato: {date}</p>
-                    <p className='time'> Tidspunkt: {timeslot}</p>
-                </div>
-
-                <div className='ud-knapper'>
-
-                    <div className='update-knap'>
-                        <img src={update} alt="update" />
-                    </div>
-
-                    <div className='delete-knap' onClick={handleDelete}>
-                        <img src={deleteButton} alt="delete" />
-                    </div>
-
-                </div>
-
-            </div>
-
+export default function Booking({ timeslot, room, date, id }) {
+  return (
+    <div className="mybookingsBox">
+      <div className="newboooking">
+        <div className="mybookings-date">
+          <h1>{room}</h1>
         </div>
-        
-    )
+
+        <div className="mybookings-info">
+          <p className="date"> Dato: {date}</p>
+          <p className="time"> Tidspunkt: {timeslot}</p>
+        </div>
+
+        <div className="ud-knapper">
+          <div className="update-knap">
+            <img src={update} alt="update" />
+          </div>
+
+          <div>
+            <Delete 
+            id={id}/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Components/molecules/Delete.jsx
+++ src/Components/molecules/Delete.jsx
@@ -1,16 +1,31 @@
-import { useEffect, useState } from "react";
-import { transformToArray } from "./firebase-utils.js";
-import SingleBooking from "./SingleBooking.jsx";
+// import { useEffect, useState } from "react";
+// import { transformToArray } from "./firebase-utils.js";
+// import SingleBooking from "./SingleBooking.jsx";
 
-const url = "https://frontend-eksamensprojekt-default-rtdb.europe-west1.firebasedatabase.app/bookings.json";
+// const url = "https://frontend-eksamensprojekt-default-rtdb.europe-west1.firebasedatabase.app/bookings.json";
 
+// export default function DeleteBooking({bookings, setBookings}){
+//     return(
+//         console.log("clicked")
+//     );
+// }
 
+import "./CSS/NewBooking.css";
+import deleteButton from "../atoms/deleteButton.png";
 
-export default function DeleteBooking({bookings, setBookings}){
-    return(
-        console.log("clicked")
+export default function Delete({ id }) {
+  const handleDelete = async () => {
+    const response = await fetch(
+      `https://frontend-eksamensprojekt-default-rtdb.europe-west1.firebasedatabase.app/bookings/${id}.json`,
+      {
+        method: "DELETE",
+      }
     );
+    window.location.reload();
+  };
+  return (
+    <div className="delete-knap" onClick={handleDelete}>
+      <img src={deleteButton} alt="delete" />
+    </div>
+  );
 }
-
-
-
